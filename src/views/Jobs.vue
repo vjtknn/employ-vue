9be40ddebb8f3716dd993fe08e5e36e63901083b
Wojciech Winnicki <wbwinnicki@gmail.com>
2018-07-07 17:46:27 +0200

    Handle input value

---
+++ src/views/Jobs.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="wrapper">
+    <div class="jobs">
+      <h1>Jobs</h1>
+      <label for="jobs"></label>
+      <input
+      id='jobs'
+      name='jobs'
+      v-model='jobsValue'
+      @input='handleInput'
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Jobs',
+  data() {
+    return {
+      jobsValue: '',
+    };
+  },
+  methods: {
+    handleInput() {
+      console.log(this.jobsValue);
+    },
+  },
+};
+</script>
+
+<style lang='scss' scoped>
+.wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0;
+  padding: 30px;
+  width: 100%;
+}
+</style>
